49e1b6d9063ae27842245f13a7c4108c3892d8bc
Andres Aparicio <andresmijin@gmail.com>
2020-06-19 16:52:54 -0400

    Log4j

---
+++ Evaluation/src/main/java/com/revature/util/LogThis.java
@@ -0,0 +1,37 @@
+package com.revature.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogThis {
+	
+	static Logger Logger = LogManager.getLogger();
+	
+	public static void LogIt(String level, String message) {
+		switch (level) {
+		case "debug":
+			Logger.debug(message);
+			break;
+		case "warn":
+			Logger.warn(message);
+			break;
+		case "error":
+			Logger.error(message);
+			break;
+		case "fatal":
+			Logger.fatal(message);
+			break;
+		case "info":
+			Logger.info(message);
+			break;
+		case "trace":
+			Logger.trace(message);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+}
+
