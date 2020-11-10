05400705419dddf2e487b83ccd93d731d3f6023e
thecheatisgod@gmail.com <thecheatisgod@gmail.com>
2020-11-10 15:33:18 -0700

    kubernetes setup

--- Evaluation/src/main/java/com/revature/EvaluationApplication.java
+++ Evaluation/src/main/java/com/revature/EvaluationApplication.java
@@ -2,8 +2,10 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
+@RibbonClient(name = "rss-evaluation-service")
 public class EvaluationApplication {
 
 	public static void main(String[] args) {
